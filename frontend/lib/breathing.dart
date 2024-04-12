7d7cde0505d04f7799d54291f4d396eb074791b8
Sabir Saheel <sahee002@d.umn.edu>
2024-04-12 10:47:36 -0500

    Added start button but the animation moves with the button

--- frontend/lib/breathing.dart
+++ frontend/lib/breathing.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/utils.dart';
 
 class BreathingScreen extends StatefulWidget {
   @override
@@ -8,7 +9,7 @@ class BreathingScreen extends StatefulWidget {
 class _BreathingScreenState extends State<BreathingScreen> with TickerProviderStateMixin {
   // AnimationController _breathingController();
   var _breathe = 0.0;
-
+  bool trigger = false;
   @override
   void initState() {
     super.initState();
@@ -16,16 +17,18 @@ class _BreathingScreenState extends State<BreathingScreen> with TickerProviderSt
     AnimationController _breathingController = AnimationController(
         vsync: this, duration: Duration(milliseconds: 4000));
     _breathingController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        // Hold the animation for 4 seconds
-        Future.delayed(Duration(seconds: 4), () {
-          _breathingController.reverse();
-        });
-      } else if (status == AnimationStatus.dismissed) {
+      if(trigger == true){
+        if (status == AnimationStatus.completed) {
+          // Hold the animation for 4 seconds
+          Future.delayed(Duration(seconds: 4), () {
+            _breathingController.reverse();
+          });
+        } else if (status == AnimationStatus.dismissed) {
 
-        Future.delayed(Duration(seconds: 4), () {
-          _breathingController.forward();
-        });
+          Future.delayed(Duration(seconds: 4), () {
+            _breathingController.forward();
+          });
+        }
       }
     });
 
@@ -46,20 +49,42 @@ class _BreathingScreenState extends State<BreathingScreen> with TickerProviderSt
     return Scaffold(
       backgroundColor: Colors.grey[850],
       body: Center(
-        child: Container(
-          height: size,
-          width: size,
-          child: Material(
-            borderRadius: BorderRadius.circular(size),
-            color: Colors.white,
-            child: Icon(
-              Icons.circle,
-              size: 100,
-              color: Colors.grey[850],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: size,
+              width: size,
+              child: Material(
+                borderRadius: BorderRadius.circular(size),
+                color: Colors.white,
+                child: Icon(
+                  Icons.circle,
+                  size: 100,
+                  color: Colors.grey[850],
 
+                ),
+              ),
             ),
-
-          ),
+            Flexible(
+                flex: 0,
+                child: InkWell(
+                  onTap: () {
+                    setState(() {
+                      trigger = true;
+                    });
+                  },
+                  child: Card(
+                      color: Colors.blue,
+                      child: Center(
+                          child: Text(
+                            "Start",
+                            style: const TextStyle(color: Colors.white),
+                            // fontSize: 24
+                          ))),
+                )),
+          ],
         ),
       ),
 
