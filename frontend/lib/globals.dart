29df0332fdcd64b39e41ba77d5ce5a9287113992
Jack Quigley <quigl088@d.umn.edu>
2024-04-12 15:36:20 -0500

    Added Change (Username/Picture/Password) buttons, Notifications and Dark mode switches, and a WorkInProgress AlertDialogue for unfinished sections

--- frontend/lib/globals.dart
+++ frontend/lib/globals.dart
@@ -1,8 +1,9 @@
 library project.globals;
 import 'package:flutter/material.dart';
-
+import 'package:frontend/utils.dart';
 
 bool darkTheme = true;
+bool notifications = false;
 
 // Background colors
 Color? dark_background = Colors.grey[850];
@@ -18,3 +19,23 @@ Color? light_text = Colors.grey[150];
 
 
 void flipTheme() { darkTheme = !darkTheme; }
+
+// Function to return a standardized WIP (Work In Progress) Alert Dialogue
+AlertDialog getWIPAlert(BuildContext context){
+  return AlertDialog(
+    title: const Text('In development!', style: TextStyle(color: Colors.white)),
+    content: const Text('This feature is currently being developed, please be patient in the meantime.\nThank you!', style: TextStyle(color: Colors.white)),
+    backgroundColor: CustomColor.purple,
+    shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(32.0))),
+    actions: <Widget>[
+      // TextButton(
+      //   onPressed: () => Navigator.pop(context, 'Cancel'),
+      //   child: const Text('Cancel'),
+      // ),
+      TextButton(
+        onPressed: () => Navigator.pop(context, 'OK'),
+        child: const Text('OK', style: TextStyle(color: Colors.white, fontSize: 20),),
+      ),
+    ],
+  );
+}
